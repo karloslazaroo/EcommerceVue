087df371b481a0966c772ab9b1380529206658e5
karloslazaroo <karloplazaro@yahoo.com>
2024-05-13 00:29:27 +0800

    Update Admin.vue

--- ecommerce-vue/ecommerce-vue/src/components/Admin.vue
+++ ecommerce-vue/ecommerce-vue/src/components/Admin.vue
@@ -1,31 +1,28 @@
 <template>
-    <form @submit.prevent="submitForm">
-      <div>
-        <label for="name">Product Name:</label>
-        <input type="text" id="name" v-model="formData.product_name">
+    <div>
+      <h2>Admin Page</h2>
+      <form @submit.prevent="submitForm">
+        <!-- Add product form fields -->
+        <input type="text" v-model="formData.product_name" placeholder="Product Name">
+        <input type="text" v-model="formData.thumbnail" placeholder="Thumbnail URL">
+        <input type="number" v-model="formData.quantity" placeholder="Quantity">
+        <input type="text" v-model="formData.description" placeholder="Description">
+        <input type="text" v-model="formData.size" placeholder="Size">
+        <input type="number" v-model="formData.price" placeholder="Price">
+  
+        <button type="submit">Add Product</button>
+      </form>
+  
+      <!-- Display existing products -->
+      <div v-if="products.length">
+        <h3>Existing Products</h3>
+        <ul>
+          <li v-for="product in products" :key="product._id">
+            {{ product.product_name }} - {{ product.price }}
+          </li>
+        </ul>
       </div>
-      <div>
-        <label for="quantity">Quantity:</label>
-        <input type="number" id="quantity" v-model="formData.quantity">
-      </div>
-      <div>
-        <label for="description">Description:</label>
-        <textarea id="description" v-model="formData.description"></textarea>
-      </div>
-      <div>
-        <label for="price">Price:</label>
-        <input type="number" id="price" v-model="formData.price">
-      </div>
-      <div>
-        <label for="size">Size:</label>
-        <input type="number" id="size" v-model="formData.size">
-      </div>
-      <div>
-        <label for="image">Image:</label>
-        <input type="file" id="image" ref="thumbnail">
-      </div>
-      <button type="submit">Submit</button>
-    </form>
+    </div>
   </template>
   
   <script>
@@ -34,38 +31,49 @@
   export default {
     data() {
       return {
+        products: [],
         formData: {
           product_name: '',
+          thumbnail: '',
           quantity: 0,
           description: '',
-          price: 0.0,
           size: '',
-          thumbnail: '' // Store the image path here
+          price: 0
         }
       };
     },
+    created() {
+      this.fetchProducts();
+    },
     methods: {
-      async submitForm() {
-        const formData = new FormData();
-        formData.append('product_name', this.formData.product_name);
-        formData.append('quantity', this.formData.quantity);
-        formData.append('description', this.formData.description);
-        formData.append('price', this.formData.price);
-        formData.append('size', this.formData.size);
-        formData.append('image', this.$refs.thumbnail.files[0]);
-  
-        try {
-          const response = await axios.post('http://localhost:4000/api/product', formData, {
-            headers: {
-              'Content-Type': 'multipart/form-data'
-            }
+      fetchProducts() {
+        axios.get('http://localhost:4000/api/product')
+          .then(response => {
+            this.products = response.data;
+          })
+          .catch(error => {
+            console.error('Error fetching products:', error);
           });
-          // Assuming the response contains the path where the image is stored
-          this.formData.thumbnail = response.data.thumbnail;
-          console.log('Product added successfully:', this.formData);
-        } catch (error) {
-          console.error('Error adding product:', error);
-        }
+      },
+      submitForm() {
+        axios.post('http://localhost:4000/api/product', this.formData)
+          .then(response => {
+            console.log('Product added successfully:', response.data);
+            // Clear form fields after successful submission
+            this.formData = {
+              product_name: '',
+              thumbnail: '',
+              quantity: 0,
+              description: '',
+              size: '',
+              price: 0
+            };
+            // Update product list
+            this.fetchProducts();
+          })
+          .catch(error => {
+            console.error('Error adding product:', error);
+          });
       }
     }
   };
